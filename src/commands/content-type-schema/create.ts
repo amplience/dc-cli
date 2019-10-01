0baea352fba4e7c33f127eb8745c4f2fda4a7455
Chris Bibby <cbibby@amplience.com>
2019-10-01 13:58:22 +0100

    feat(content-type-schema create): create content type schema

---
+++ src/commands/content-type-schema/create.ts
@@ -0,0 +1,76 @@
+import { CommandOptions } from '../../interfaces/command-options.interface';
+import { renderData, RenderingArguments, RenderingOptions } from '../../view/data-presenter';
+import { Arguments } from 'yargs';
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+import { ConfigurationParameters } from '../configure';
+import * as fs from 'fs';
+import { ValidationLevel, ContentTypeSchema } from 'dc-management-sdk-js';
+import { URL } from 'url';
+import axios from 'axios';
+
+export const command = 'create';
+
+export const desc = 'Create Content Type Schema';
+
+export const builder: CommandOptions = {
+  schema: {
+    type: 'string',
+    demandOption: true,
+    description: 'content-type-schema Source Location'
+  },
+  validationLevel: {
+    type: 'string',
+    choices: Object.values(ValidationLevel),
+    demandOption: true,
+    description: 'content-type-schema Validation Level'
+  },
+  ...RenderingOptions
+};
+
+interface BuilderOptions {
+  schema: string;
+  validationLevel: string;
+}
+
+async function getSchemaBody(schema: string): Promise<string> {
+  if (schema.match(/http?s:\/\//)) {
+    const result = await axios.get(schema);
+    if (typeof result.data == 'string') {
+      return result.data;
+    }
+
+    return JSON.stringify(result.data);
+  }
+
+  const path = schema.match(/file:\/\//) ? new URL(schema) : schema;
+  const schemaBody = fs.readFileSync(path, 'utf-8');
+  return schemaBody;
+}
+
+export const handler = async (
+  argv: Arguments<BuilderOptions & ConfigurationParameters & RenderingArguments>
+): Promise<void> => {
+  const schemaBody = await getSchemaBody(argv.schema);
+
+  const schemaJson = JSON.parse(schemaBody);
+  if (schemaJson.id == undefined) {
+    throw new Error('Missing id from schema');
+  }
+
+  const contentTypeSchema = new ContentTypeSchema();
+  contentTypeSchema.body = schemaBody;
+  contentTypeSchema.schemaId = schemaJson.id;
+  contentTypeSchema.validationLevel = argv.validationLevel as ValidationLevel;
+
+  const client = dynamicContentClientFactory(argv);
+  const hub = await client.hubs.get(argv.hubId);
+  const contentTypeSchemaResult = await hub.related.contentTypeSchema.create(contentTypeSchema);
+
+  return renderData(argv, contentTypeSchemaResult, undefined, {
+    columns: {
+      1: {
+        width: 100
+      }
+    }
+  });
+};
