14ee22eae53f27f10bd38e1dec057a51064d3fd3
Ben Pearey <bpearey@amplience.com>
2019-10-02 17:42:40 +0100

    feat(content type): adding register content type command

---
+++ src/commands/content-type/register.spec.ts
@@ -0,0 +1,165 @@
+import { handler } from './register';
+import DataPresenter from '../../view/data-presenter';
+import { ContentType } from 'dc-management-sdk-js';
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+
+jest.mock('../../services/dynamic-content-client-factory');
+jest.mock('../../view/data-presenter');
+const mockDataPresenter = DataPresenter as jest.Mock<DataPresenter<ContentType>>;
+
+describe('Content type register', () => {
+  let mockRegister: jest.Mock;
+  let mockGetHub: jest.Mock;
+
+  beforeEach(() => {
+    mockRegister = jest.fn();
+    mockGetHub = jest.fn().mockResolvedValue({
+      related: {
+        contentTypes: {
+          register: mockRegister
+        }
+      }
+    });
+    (dynamicContentClientFactory as jest.Mock).mockReturnValue({
+      hubs: {
+        get: mockGetHub
+      }
+    });
+  });
+
+  it('should register content type with icons and visualization', async () => {
+    const yargArgs = {
+      $0: 'src/index.ts',
+      _: ['content-type', 'register']
+    };
+    const config = {
+      clientId: 'client-id',
+      clientSecret: 'client-id',
+      hubId: 'hub-id'
+    };
+    const registerArgs = {
+      label: 'test-label',
+      schemaId: 'schema-id',
+      icons: {
+        '0': {
+          size: 256,
+          url: 'test-url'
+        }
+      },
+      visualizations: {
+        '0': {
+          label: 'viz-label-1',
+          templatedUri: 'viz-test-url-1',
+          default: true
+        }
+      }
+    };
+    const argv = { ...yargArgs, ...config, ...registerArgs };
+
+    const registerResponse = new ContentType({
+      contentTypeUri: registerArgs.schemaId,
+      settings: {
+        label: registerArgs.label,
+        icons: [
+          {
+            size: 256,
+            url: 'test-url'
+          }
+        ],
+        visualizations: [
+          {
+            label: 'viz-label-1',
+            templatedUri: 'viz-test-url-1',
+            default: true
+          }
+        ]
+      }
+    });
+
+    mockRegister.mockResolvedValue(registerResponse);
+
+    await handler(argv);
+
+    expect(mockGetHub).toBeCalledWith('hub-id');
+    expect(mockRegister).toHaveBeenCalledWith(expect.objectContaining(registerResponse.toJson()));
+    expect(mockDataPresenter).toHaveBeenCalledWith(argv, registerResponse);
+    expect(mockDataPresenter.mock.instances[0].render).toHaveBeenCalled();
+  });
+
+  it('should register a content type with ordered icon and visualization', async () => {
+    const yargArgs = {
+      $0: 'src/index.ts',
+      _: ['content-type', 'register']
+    };
+    const config = {
+      clientId: 'client-id',
+      clientSecret: 'client-id',
+      hubId: 'hub-id'
+    };
+    const registerArgs = {
+      label: 'test-label',
+      schemaId: 'schema-id',
+      icons: {
+        '1': {
+          size: 512,
+          url: 'test-url2'
+        },
+        '0': {
+          size: 256,
+          url: 'test-url1'
+        }
+      },
+      visualizations: {
+        '1': {
+          label: 'viz-label-2',
+          templatedUri: 'viz-test-url-2',
+          default: false
+        },
+        '0': {
+          label: 'viz-label-1',
+          templatedUri: 'viz-test-url-1',
+          default: true
+        }
+      }
+    };
+    const argv = { ...yargArgs, ...config, ...registerArgs };
+
+    const registerResponse = new ContentType({
+      contentTypeUri: registerArgs.schemaId,
+      settings: {
+        label: registerArgs.label,
+        icons: [
+          {
+            size: 256,
+            url: 'test-url1'
+          },
+          {
+            size: 512,
+            url: 'test-url2'
+          }
+        ],
+        visualizations: [
+          {
+            label: 'viz-label-1',
+            templatedUri: 'viz-test-url-1',
+            default: true
+          },
+          {
+            label: 'viz-label-2',
+            templatedUri: 'viz-test-url-2',
+            default: false
+          }
+        ]
+      }
+    });
+
+    mockRegister.mockResolvedValue(registerResponse);
+
+    await handler(argv);
+
+    expect(mockGetHub).toBeCalledWith('hub-id');
+    expect(mockRegister).toHaveBeenCalledWith(expect.objectContaining(registerResponse.toJson()));
+    expect(mockDataPresenter).toHaveBeenCalledWith(argv, registerResponse);
+    expect(mockDataPresenter.mock.instances[0].render).toHaveBeenCalled();
+  });
+});
---
+++ src/commands/content-type/register.ts
@@ -0,0 +1,69 @@
+import { CommandOptions } from '../../interfaces/command-options.interface';
+import DataPresenter, { RenderingOptions, RenderingArguments } from '../../view/data-presenter';
+import { Arguments } from 'yargs';
+import { ConfigurationParameters } from '../configure';
+import { ContentType, ContentTypeIcon, ContentTypeVisualization } from 'dc-management-sdk-js';
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+
+export const command = 'register';
+
+export const desc = 'Register a Content Type';
+
+export const builder: CommandOptions = {
+  schemaId: {
+    type: 'string',
+    demandOption: true,
+    description: 'content-type-schema ID'
+  },
+  label: {
+    type: 'string',
+    demandOption: true,
+    description: 'Content type label'
+  },
+  icons: {
+    description: 'Content type icons',
+    default: {}
+  },
+  visualizations: {
+    description: 'Content type visualizations',
+    default: {}
+  },
+  ...RenderingOptions
+};
+
+interface IconOption {
+  [key: string]: ContentTypeIcon;
+}
+
+interface VisualizationOption {
+  [key: string]: ContentTypeVisualization;
+}
+
+interface ContentTypeBuilderOptions {
+  icons: IconOption;
+  visualizations: VisualizationOption;
+}
+
+function parseSettingsArg(objectArg: IconOption | VisualizationOption): ContentTypeIcon[] | ContentTypeVisualization[] {
+  return Object.entries(objectArg)
+    .sort((a, b) => (parseInt(a[0], 10) > parseInt(b[0], 10) ? 1 : -1))
+    .map(entry => entry[1]);
+}
+
+export const handler = async (
+  argv: Arguments<ContentTypeBuilderOptions & ConfigurationParameters & RenderingArguments>
+): Promise<void> => {
+  const client = dynamicContentClientFactory(argv);
+  const hub = await client.hubs.get(argv.hubId);
+  const contentType = new ContentType({
+    contentTypeUri: argv.schemaId,
+    settings: {
+      label: argv.label,
+      icons: parseSettingsArg(argv.icons),
+      visualizations: parseSettingsArg(argv.visualizations)
+    }
+  });
+  const registeredContentType = await hub.related.contentTypes.register(contentType);
+
+  new DataPresenter(argv, registeredContentType).render();
+};
