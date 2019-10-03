e1ff15561358584d1ae6205414c5407702fb5c96
Sean Ives <sives@amplience.com>
2019-10-03 11:15:11 +0100

    feat(content type schema update): ability to update a content type schema

---
+++ src/commands/content-type-schema/update.ts
@@ -0,0 +1,82 @@
+import { CommandOptions } from '../../interfaces/command-options.interface';
+import DataPresenter, { RenderingArguments, RenderingOptions } from '../../view/data-presenter';
+import { Arguments } from 'yargs';
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+import { ConfigurationParameters } from '../configure';
+import * as fs from 'fs';
+import { ValidationLevel, ContentTypeSchema } from 'dc-management-sdk-js';
+import { URL } from 'url';
+import axios from 'axios';
+
+export const command = 'update';
+
+export const desc = 'Update Content Type Schema';
+
+export const builder: CommandOptions = {
+  id: {
+    type: 'string',
+    demandOption: true,
+    description: 'content-type-schema ID'
+  },
+  schema: {
+    type: 'string',
+    demandOption: true,
+    description: 'content-type-schema Source Location'
+  },
+  validationLevel: {
+    type: 'string',
+    choices: Object.values(ValidationLevel),
+    demandOption: true,
+    description: 'content-type-schema Validation Level'
+  },
+  ...RenderingOptions
+};
+
+export interface BuilderOptions {
+  id: string;
+  schema: string;
+  validationLevel: string;
+}
+
+async function getSchemaBody(schema: string): Promise<string> {
+  if (schema.match(/^(http|https):\/\//)) {
+    const result = await axios.get(schema);
+
+    if (typeof result.data == 'string') {
+      return result.data;
+    }
+
+    return JSON.stringify(result.data);
+  }
+
+  const path = schema.match(/file:\/\//) ? new URL(schema) : schema;
+  const schemaBody = fs.readFileSync(path, 'utf-8');
+  return schemaBody;
+}
+
+export const handler = async (
+  argv: Arguments<BuilderOptions & ConfigurationParameters & RenderingArguments>
+): Promise<void> => {
+  const schemaBody = await getSchemaBody(argv.schema);
+  const schemaJson = JSON.parse(schemaBody);
+  if (schemaJson.id == undefined) {
+    throw new Error('Missing id from schema');
+  }
+
+  const client = dynamicContentClientFactory(argv);
+
+  const updatedSchema = new ContentTypeSchema();
+  updatedSchema.body = schemaBody;
+  updatedSchema.validationLevel = argv.validationLevel as ValidationLevel;
+
+  const contentTypeSchema = await client.contentTypeSchemas.get(argv.id);
+  const contentTypeSchemaResult = await contentTypeSchema.related.update(updatedSchema);
+
+  return new DataPresenter(argv, contentTypeSchemaResult, {
+    columns: {
+      1: {
+        width: 100
+      }
+    }
+  }).render();
+};
