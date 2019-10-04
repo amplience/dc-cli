118b48670a619c9cd8303ff322b6b0af66c8452d
Simon Mudd <smudd@amplience.com>
2019-10-04 09:19:54 +0100

    feat(content repositories): added functionality to unassign a content type along with tests

---
+++ src/commands/content-repositories/unassign-content-type.spec.ts
@@ -0,0 +1,96 @@
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+import { builder, command, handler } from './unassign-content-type';
+import DataPresenter, { RenderingOptions } from '../../view/data-presenter';
+import { ContentRepository } from 'dc-management-sdk-js';
+import { singleItemTableOptions } from '../../common/table/table.consts';
+import Yargs from 'yargs/yargs';
+
+jest.mock('../../services/dynamic-content-client-factory');
+jest.mock('../../view/data-presenter');
+
+describe('content-repository assign-content-type command', () => {
+  const mockDataPresenter = DataPresenter as jest.Mock<DataPresenter>;
+
+  afterEach((): void => {
+    jest.restoreAllMocks();
+  });
+
+  it('should have a command defined', function() {
+    expect(command).toEqual('$0 <id> unassign-content-type');
+  });
+
+  describe('builder tests', function() {
+    it('should configure yargs', function() {
+      const argv = Yargs(process.argv.slice(2));
+      const spyPositional = jest.spyOn(argv, 'positional').mockReturnThis();
+      const spyOptions = jest.spyOn(argv, 'options').mockReturnThis();
+
+      const options = {
+        contentTypeId: {
+          type: 'string',
+          demandOption: true,
+          description: 'content-type ID to unassign'
+        },
+        ...RenderingOptions
+      };
+
+      builder(argv);
+
+      expect(spyPositional).toHaveBeenCalledWith('id', {
+        demandOption: true,
+        describe: 'Content Repository ID',
+        type: 'string'
+      });
+      expect(spyOptions).toHaveBeenCalledWith(options);
+    });
+  });
+
+  describe('handler tests', function() {
+    const yargArgs = {
+      $0: 'test',
+      _: ['test'],
+      json: true,
+      contentTypeId: 'content-type-id'
+    };
+    const config = {
+      clientId: 'client-id',
+      clientSecret: 'client-id',
+      hubId: 'hub-id'
+    };
+
+    it('should return a content repository', async () => {
+      const mockGet = jest.fn();
+      const mockUnassign = jest.fn();
+      const mockToJSON = jest.fn();
+      (dynamicContentClientFactory as jest.Mock).mockReturnValue({
+        contentRepositories: {
+          get: mockGet
+        }
+      });
+      mockGet.mockResolvedValue({
+        related: {
+          contentTypes: {
+            unassign: mockUnassign
+          }
+        },
+        toJSON: mockToJSON
+      });
+
+      const contentRepository = new ContentRepository({
+        contentTypes: []
+      });
+      mockUnassign.mockResolvedValue(undefined);
+      mockToJSON.mockReturnValue(contentRepository.toJSON());
+
+      const argv = { ...yargArgs, id: 'content-type-id', ...config };
+      await handler(argv);
+
+      expect(mockToJSON).toHaveBeenCalled();
+      expect(mockDataPresenter).toHaveBeenCalledWith(contentRepository.toJSON());
+      expect(mockDataPresenter.mock.instances[0].render).toHaveBeenCalledWith({
+        json: argv.json,
+        tableUserConfig: singleItemTableOptions
+      });
+    });
+  });
+});
---
+++ src/commands/content-repositories/unassign-content-type.ts
@@ -0,0 +1,46 @@
+import DataPresenter, { RenderingArguments, RenderingOptions } from '../../view/data-presenter';
+import { Arguments, Argv } from 'yargs';
+import dynamicContentClientFactory from '../../services/dynamic-content-client-factory';
+import { ConfigurationParameters } from '../configure';
+import { singleItemTableOptions } from '../../common/table/table.consts';
+
+export const command = '$0 <id> unassign-content-type';
+
+export const desc = 'Unassign Content Type';
+
+export const builder = (yargs: Argv): void => {
+  yargs
+    .positional('id', {
+      describe: 'Content Repository ID',
+      type: 'string',
+      demandOption: true
+    })
+    .options({
+      contentTypeId: {
+        type: 'string',
+        demandOption: true,
+        description: 'content-type ID to unassign'
+      },
+      ...RenderingOptions
+    });
+};
+
+export interface BuilderOptions {
+  id: string;
+  contentTypeId: string;
+}
+
+export const handler = async (
+  argv: Arguments<BuilderOptions & ConfigurationParameters & RenderingArguments>
+): Promise<void> => {
+  const client = dynamicContentClientFactory(argv);
+
+  const contentRepository = await client.contentRepositories.get(argv.id);
+  await contentRepository.related.contentTypes.unassign(argv.contentTypeId);
+  const contentRepositoryUpdated = await client.contentRepositories.get(argv.id);
+
+  return new DataPresenter(contentRepositoryUpdated.toJSON()).render({
+    json: argv.json,
+    tableUserConfig: singleItemTableOptions
+  });
+};
