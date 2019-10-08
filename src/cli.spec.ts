30d22b03438a030619be5965ee2095a57ea3bdd7
Marc Easen <measen@amplience.com>
2019-10-08 14:10:17 +0100

    feat(error handling): errors are now reported without the stack trace

--- src/cli.spec.ts
+++ src/cli.spec.ts
@@ -1,4 +1,4 @@
-import cli from './cli';
+import * as cli from './cli';
 import Yargs from 'yargs/yargs';
 import { configureCommandOptions } from './commands/configure';
 import YargsCommandBuilderOptions from './common/yargs/yargs-command-builder-options';
@@ -7,14 +7,32 @@ import { basename } from 'path';
 jest.mock('./commands/configure');
 
 describe('cli', (): void => {
-  it('should configure yarg instance', (): void => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  const captureConsole = async (methodToWrap: () => void): Promise<string> => {
+    let buffer = '';
+    jest.spyOn(console, 'error').mockImplementation((output: string) => (buffer = `${buffer}${output}`));
+    await methodToWrap();
+    // replace config file and script entry point
+    return buffer.replace(new RegExp(basename(process.argv[1]), 'g'), '<<ENTRYPOINT>>');
+  };
+
+  const throwErrorAndCaptureConsole = async (error: { message: string } | string | Error): Promise<string> => {
+    return await captureConsole(async () => {
+      await cli.displayError(error);
+    });
+  };
+
+  it('should configure yarg instance', async (): Promise<void> => {
     const argv = Yargs(process.argv.slice(2));
     const spyOptions = jest.spyOn(argv, 'options').mockReturnThis();
     const spyConfig = jest.spyOn(argv, 'config').mockReturnThis();
     const spyCommandDir = jest.spyOn(argv, 'commandDir').mockReturnThis();
     const spyDemandCommand = jest.spyOn(argv, 'demandCommand').mockReturnValue(argv);
 
-    cli(argv);
+    await cli.default(argv);
 
     expect(spyOptions).toHaveBeenCalledWith(configureCommandOptions);
     expect(spyConfig).toHaveBeenCalledWith('config', expect.any(Function));
@@ -22,18 +40,37 @@ describe('cli', (): void => {
     expect(spyDemandCommand).toHaveBeenCalledWith(1, 'Please specify at least one command');
   });
 
-  it('should create a yarg instance if one is not supplied', () => {
+  it('should create a yarg instance if one is not supplied', async () => {
     configureCommandOptions.config.default = 'config.json';
-    let buffer = '';
     const processExitSpy = jest.spyOn(process, 'exit').mockImplementation();
-    jest.spyOn(console, 'error').mockImplementation((output: string) => (buffer = `${buffer}${output}`));
 
-    cli();
+    const buffer = await captureConsole(async () => {
+      await cli.default();
+    });
 
-    // replace config file and script entry point
-    buffer = buffer.replace(new RegExp(basename(process.argv[1]), 'g'), '<<ENTRYPOINT>>');
-
-    expect(processExitSpy).toHaveBeenCalled();
     expect(buffer).toMatchSnapshot();
+    expect(processExitSpy).not.toHaveBeenCalled();
+  });
+
+  it('should display sdk error (string)', async () => {
+    const spyDisplayError = jest.spyOn(cli, 'displayError');
+    expect(
+      await throwErrorAndCaptureConsole(
+        'The list-content-type-schemas action is not available, ensure you have permission to perform this action.'
+      )
+    ).toMatchSnapshot();
+    expect(spyDisplayError).toHaveBeenCalled();
+  });
+
+  it('should display sdk error ({message})', async () => {
+    const spyDisplayError = jest.spyOn(cli, 'displayError');
+    expect(await throwErrorAndCaptureConsole({ message: 'Error message' })).toMatchSnapshot();
+    expect(spyDisplayError).toHaveBeenCalled();
+  });
+
+  it('should display sdk error (Error)', async () => {
+    const spyDisplayError = jest.spyOn(cli, 'displayError');
+    expect(await throwErrorAndCaptureConsole(new Error('Error'))).toMatchSnapshot();
+    expect(spyDisplayError).toHaveBeenCalled();
   });
 });
