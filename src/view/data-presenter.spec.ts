fa1445f366755e472b4cc7183f3fdd50ba0ae813
Marc Easen <measen@amplience.com>
2019-10-09 09:05:21 +0100

    refactor(data-presenter): refactored the table generation and push down the empty list support

--- src/view/data-presenter.spec.ts
+++ src/view/data-presenter.spec.ts
@@ -36,15 +36,6 @@ describe('DataPresenter', (): void => {
       expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
     });
 
-    it('should not output page information for a single item', (): void => {
-      const page = {
-        number: 0,
-        totalPages: 20
-      };
-      new DataPresenter(singleItem, page).render();
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
-    });
-
     it('should render a single item as vertical table using tableUserConfig', (): void => {
       new DataPresenter(singleItem).render({
         tableUserConfig: {
@@ -59,52 +50,76 @@ describe('DataPresenter', (): void => {
     });
   });
 
-  describe('collection of items', function() {
-    const collectionOfItems: TestItem[] = [
-      {
-        foo: 'bar1',
-        key: 'value1 '.repeat(20)
-      },
-      {
-        foo: 'bar2',
-        key: 'value2 '.repeat(20)
-      }
-    ];
-    it('should render a collection of items in a horizontal table', (): void => {
-      new DataPresenter(collectionOfItems).render();
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
-    });
+  describe('collection of items tests', function() {
+    describe('collection of items', function() {
+      const collectionOfItems: TestItem[] = [
+        {
+          foo: 'bar1',
+          key: 'value1 '.repeat(20)
+        },
+        {
+          foo: 'bar2',
+          key: 'value2 '.repeat(20)
+        }
+      ];
 
-    it('should render a collection of items as json', (): void => {
-      new DataPresenter(collectionOfItems).render({ json: true });
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
-    });
+      it('should render a collection of items in a horizontal table', (): void => {
+        new DataPresenter(collectionOfItems).render();
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
 
-    it('should render using the supplied map() for a collection of items in a horizontal table', (): void => {
-      new DataPresenter(collectionOfItems).render({ itemMapFn: ({ foo }: TestItem): object => ({ foo }) });
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
-    });
+      it('should render a collection of items as json', (): void => {
+        new DataPresenter(collectionOfItems).render({ json: true });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
 
-    it('should render a collection of items horizontal table with some page information', (): void => {
-      const page = {
-        number: 0,
-        totalPages: 20
-      };
-      new DataPresenter(collectionOfItems, page).render();
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
-    });
+      it('should render using the supplied map() for a collection of items in a horizontal table', (): void => {
+        new DataPresenter(collectionOfItems).render({ itemMapFn: ({ foo }: TestItem): object => ({ foo }) });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
 
-    it('should render a collection of items horizontal table using with table config', (): void => {
-      new DataPresenter(collectionOfItems).render({
-        tableUserConfig: {
-          columns: {
-            1: {
-              width: 100
+      it('should render a collection of items horizontal table using with table config', (): void => {
+        new DataPresenter(collectionOfItems).render({
+          tableUserConfig: {
+            columns: {
+              1: {
+                width: 100
+              }
             }
           }
-        }
+        });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
       });
-      expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+    });
+
+    describe('collection of 0 items', function() {
+      it('should render a collection of items in a horizontal table', (): void => {
+        new DataPresenter([]).render();
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
+
+      it('should render a collection of items as json', (): void => {
+        new DataPresenter([]).render({ json: true });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
+
+      it('should render using the supplied map() for a collection of items in a horizontal table', (): void => {
+        new DataPresenter([]).render({ itemMapFn: ({ foo }: TestItem): object => ({ foo }) });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
+
+      it('should render a collection of items horizontal table using with table config', (): void => {
+        new DataPresenter([]).render({
+          tableUserConfig: {
+            columns: {
+              1: {
+                width: 100
+              }
+            }
+          }
+        });
+        expect(stdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
+      });
     });
   });
 });
