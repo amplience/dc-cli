94103aa32fa0485040fa579ec3e90530f4fd9918
Ben Pearey <bpearey@amplience.com>
2019-10-17 18:03:24 +0100

    test(create service): adding create service coverage

---
+++ src/commands/content-type-schema/create.service.spec.ts
@@ -0,0 +1,29 @@
+import { createContentTypeSchema } from './create.service';
+import { ValidationLevel, Hub, ContentTypeSchema } from 'dc-management-sdk-js';
+
+describe('create.service', () => {
+  describe('createContentTypeSchema', () => {
+    it('should create schema', async () => {
+      const schemaBody = { id: 'http://example.com/schema.json' };
+      const schemaToCreate = {
+        body: JSON.stringify(schemaBody),
+        validationLevel: ValidationLevel.CONTENT_TYPE,
+        schemaId: schemaBody.id
+      };
+      const mockCreate = jest.fn().mockResolvedValue(new ContentTypeSchema(schemaToCreate));
+      const mockHub = new Hub();
+      mockHub.related.contentTypeSchema.create = mockCreate;
+      const result = await createContentTypeSchema(JSON.stringify(schemaBody), ValidationLevel.CONTENT_TYPE, mockHub);
+
+      expect(mockCreate).toHaveBeenCalledTimes(1);
+      expect(mockCreate).toHaveBeenCalledWith(expect.objectContaining(schemaToCreate));
+      expect(result).toEqual(expect.objectContaining(schemaToCreate));
+    });
+
+    it('should throw and error when schemaId is missing from the schemaBody', async () => {
+      await expect(
+        createContentTypeSchema(JSON.stringify({}), ValidationLevel.CONTENT_TYPE, new Hub())
+      ).rejects.toThrowErrorMatchingSnapshot();
+    });
+  });
+});
