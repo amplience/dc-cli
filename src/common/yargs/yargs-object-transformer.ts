e6f8335924e133af148ef68ee3a8e2c08f035af6
Simon Mudd <smudd@amplience.com>
2019-10-25 14:19:31 +0100

    feat(yargs object transformer): added index checking to avoid array overwrites

--- src/common/yargs/yargs-object-transformer.ts
+++ src/common/yargs/yargs-object-transformer.ts
@@ -5,5 +5,10 @@ export interface YargObject<T> {
 // yargs returns true when omitting dot notation on YargObjects e.g. --icons
 // allowing the transformer to accept a boolean returns [] so we can empty properties that are arrays
 export const transformYargObjectToArray = <T>(yargObject: YargObject<T> | boolean): T[] => {
+  const index = Object.keys(yargObject);
+  if (index.length > 0 && index.indexOf('0') < 0) {
+    throw new Error('Targeted array indexes are unsupported, please provide a full array index starting at 0');
+  }
+
   return Object.entries(yargObject).map(entry => entry[1]);
 };
