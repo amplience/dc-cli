cd2609235df506a8517fdfb2eda8c7026667d8ec
Simon Mudd <smudd@amplience.com>
2019-10-28 09:20:56 +0000

    feat(sequential indexes): adding testing to ensure indexes are sequential

--- src/common/yargs/yargs-object-transformer.ts
+++ src/common/yargs/yargs-object-transformer.ts
@@ -2,13 +2,25 @@ export interface YargObject<T> {
   [key: string]: T;
 }
 
+const validateArrayIndex = <T>(yargObject: YargObject<T> | boolean): void => {
+  const index = Object.keys(yargObject);
+  if (index.length === 0) {
+    return;
+  }
+
+  const isIndexSequential = index
+    .sort()
+    .every((suppliedIndex: string, actualIndex: number) => parseInt(suppliedIndex) == actualIndex);
+
+  if (!isIndexSequential) {
+    throw new Error('Targeted array indexes are unsupported, please provide a full array index starting at 0');
+  }
+};
+
 // yargs returns true when omitting dot notation on YargObjects e.g. --icons
 // allowing the transformer to accept a boolean returns [] so we can empty properties that are arrays
 export const transformYargObjectToArray = <T>(yargObject: YargObject<T> | boolean): T[] => {
-  const index = Object.keys(yargObject);
-  if (index.length > 0 && index.indexOf('0') < 0) {
-    throw new Error('Targeted array indexes are unsupported, please provide a full array index starting at 0');
-  }
+  validateArrayIndex(yargObject);
 
   return Object.entries(yargObject).map(entry => entry[1]);
 };
