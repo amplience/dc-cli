3260c81bbbe10d29d0b5bc3aad5331e6ac61c571
Sean Ives <sives@amplience.com>
2019-11-07 15:13:14 +0000

    fix(content type import): allow filename of each parsed import file to be stored in memory

--- src/services/import.service.spec.ts
+++ src/services/import.service.spec.ts
@@ -6,12 +6,11 @@ interface ImportObject {
 
 describe('loadJsonFromDirectory tests', () => {
   it('should return a list of content types to import', (): void => {
-    const importObjects: ImportObject[] = loadJsonFromDirectory<ImportObject>(
+    const importObjects: [string, ImportObject][] = loadJsonFromDirectory<ImportObject>(
       __dirname + '/__fixtures__/load-json-from-directory/success/'
     );
-    expect(importObjects).toEqual([
-      JSON.parse(fs.readFileSync(__dirname + '/__fixtures__/load-json-from-directory/success/valid.json', 'utf-8'))
-    ]);
+    const filename = __dirname + '/__fixtures__/load-json-from-directory/success/valid.json';
+    expect(importObjects).toEqual([[filename, JSON.parse(fs.readFileSync(filename, 'utf-8'))]]);
   });
 
   it('should throw an error if any import file is not json', (): void => {
