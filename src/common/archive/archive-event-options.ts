6fad802e705966b5a2e80058593add865dcc2ea8
Rhys <rsimpson@amplience.com>
2021-08-17 17:13:16 +0100

    fix(facet): fix rebase issues

--- src/common/archive/archive-event-options.ts
+++ src/common/archive/archive-event-options.ts
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import { FileLog } from '../file-log';
 
-=======
->>>>>>> fix(content-item): fix event command
 export default interface ArchiveOptions {
   id?: string | string[];
 
   name?: string | string[];
 
-<<<<<<< HEAD
   logFile: FileLog;
-=======
-  logFile?: string;
->>>>>>> fix(content-item): fix event command
   force?: boolean;
   silent?: boolean;
   ignoreError?: boolean;
