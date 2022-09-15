f05d1e7219cd47f387ab01feb7e201412dd9111a
Dave Lilly <dave@lillys.org>
2022-09-15 09:33:36 -0500

    fix: novadev-694

--- src/commands/hub.ts
+++ src/commands/hub.ts
@@ -9,7 +9,10 @@ export const hubBuilder = (yargs: Argv): Argv =>
   yargs.positional('hub', {
     describe: 'hub name',
     type: 'string',
-    demandOption: false
+    demandOption: false,
+
+    // novadev-694 provide a default value so we don't choke and can present the selection list
+    default: ''
   });
 
 export const builder = (yargs: Argv): Argv =>
