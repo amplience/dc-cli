a5d8dc564df0330c231fda1c919bd47e53553c58
rs-amp <56435292+rs-amp@users.noreply.github.com>
2022-10-28 09:26:40 +0100

    feat(settings): overwrite existing preview settings rather than ignoring updates (#160)
    * feat(settings): overwrite existing preview settings rather than ignoring updates
    
    * feat(settings): new allowDelete option to allow removal of existing settings
    
    * test(settings): add documentation and tests for allowDelete option

--- src/interfaces/import-settings-builder-options.interface.ts
+++ src/interfaces/import-settings-builder-options.interface.ts
@@ -2,6 +2,7 @@ import { FileLog } from '../common/file-log';
 
 export interface ImportSettingsBuilderOptions {
   filePath: string;
+  allowDelete?: boolean;
   mapFile?: string;
   logFile: FileLog;
   force?: boolean;
