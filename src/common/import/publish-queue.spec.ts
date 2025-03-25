650a6775ff531cbb4ba225b679231147859c92b2
Michael Jacholke <101181580+mjacholke@users.noreply.github.com>
2025-03-25 10:53:25 +0000

    fix: bad merge on test file

--- src/common/import/publish-queue.spec.ts
+++ src/common/import/publish-queue.spec.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { Oauth2AuthHeaderProvider, ContentItem, AccessToken } from 'dc-management-sdk-js';
+import { Oauth2AuthHeaderProvider, ContentItem } from 'dc-management-sdk-js';
 import { PublishingJob, PublishQueue } from './publish-queue';
 import * as publishQueueModule from './publish-queue';
 
@@ -46,9 +46,9 @@ describe('publish-queue', () => {
     // should wait for all publishes to complete when calling waitForAll
 
     function sharedMock(templates: PublishTemplate[]): void {
-      (Oauth2AuthHeaderProvider.prototype.getToken as jest.Mock).mockImplementation(() => {
+      (Oauth2AuthHeaderProvider.prototype.getAuthHeader as jest.Mock).mockImplementation(() => {
         authRequests++;
-        const result: AccessToken = { access_token: 'token-example', expires_in: 99999, refresh_token: 'refresh' };
+        const result = 'bearer token-example';
         return Promise.resolve(result);
       });
 
